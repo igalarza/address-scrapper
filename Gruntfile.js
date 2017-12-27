9cfbf088177d080efd8dd27622f6988518c4d4cd
Iván Galarza Vallés <nadeyyo@gmail.com>
2017-12-27 21:25:02 +0100

    Coverage configuration updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,12 @@ module.exports = function (grunt) {
       options: {
         forceExit: true,
         coverage: {
-          includeAllSources: true
+          includeAllSources: true,
+          excludes: [
+            '*.js',
+            'test/**.*',
+            'coverage/**'
+          ]
         },
         jasmine: {
           spec_dir: 'test',
