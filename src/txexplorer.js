abb5043e23ccd1c0253487b18d84e52efab19872
Iván Galarza Vallés <nadeyyo@gmail.com>
2017-12-27 21:27:49 +0100

    Added more info to address objects

--- src/txexplorer.js
+++ src/txexplorer.js
@@ -2,9 +2,10 @@
 
 class TransactionExplorer {
 
-  constructor (logLevel, rpc, collection) {
+  constructor (logLevel, rpc, currentBlock, collection) {
     this.log = logLevel
     this.rpc = rpc
+    this.currentBlock = currentBlock
     this.collection = collection
   }
 
@@ -16,7 +17,7 @@ class TransactionExplorer {
       } else {
         if (this.log > 2) console.log('iterateTransactions. currentTx (' + currentTx + '): ' + txArray[currentTx])
         this.getTransactionInfo(txArray[currentTx])
-          .then((info) => this.iterateOutputs(info.vout))
+          .then((info) => this.iterateOutputs(info))
           .then(() => this.iterateTransactions(txArray, ++currentTx))
           .then(() => resolve())
           .catch((err) => reject(err))
@@ -24,10 +25,11 @@ class TransactionExplorer {
     })
   }
 
-  iterateOutputs (outputArray) {
+  iterateOutputs (tx) {
     return new Promise((resolve, reject) => {
-      outputArray.map((output) => {
-        let addresses = output.scriptPubKey.addresses
+      tx.vout.map((output) => {
+        let addresses = this.parseOutput(tx, output)
+        if (this.log > 3) console.log(addresses)
         if (this.log > 2) console.log('iterateOutputs. addresses: ' + addresses)
         addresses.map((address) => this.persistAddress(address))
       })
@@ -35,22 +37,41 @@ class TransactionExplorer {
     })
   }
 
-  persistAddress (address) {
-    let persistedAddress = this.collection.by('address', address);
+  parseOutput (tx, output) {
+    return output.scriptPubKey.addresses.map((address) => {
+      return {
+        address: address,
+        firstSeen: this.currentBlock,
+        lastSeen: this.currentBlock,
+        value: output.value,
+        txs: [tx.txid]
+      }
+    })
+  }
+
+  persistAddress (addressObj) {
+    let persistedAddress = this.collection.by('address', addressObj.address)
     if (typeof persistedAddress === 'undefined') {
-      this.collection.insert({address})
+      if (this.log > 1) console.log('Inserted new address: ' + addressObj.address)
+      this.collection.insert(addressObj)
     } else {
-      // TODO Update address object
+      persistedAddress.value += addressObj.value
+      persistedAddress.txs = persistedAddress.txs.concat(addressObj.txs)
+      persistedAddress.lastSeen = addressObj.lastSeen
+      if (this.log > 1) console.log('Address updated: ' + persistedAddress.address)
+      this.collection.update(persistedAddress)
     }
   }
 
   getTransactionInfo (txId) {
     if (this.log > 2) console.log('getTransactionInfo. txId: ' + txId)
     return new Promise((resolve, reject) => {
+      let that = this
       this.rpc.getRawTransaction(txId, 1, function (err, res) {
         if (err) {
           reject('Error: ' + err)
         } else {
+          if (that.log > 3) console.log(res.result)
           resolve(res.result)
         }
       })
