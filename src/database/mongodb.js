c48e192866a3eda0682c4dd7c9eefc5fe217a131
Iván Galarza Vallés <nadeyyo@gmail.com>
2018-01-03 00:39:07 +0100

    Last methods added to MongoDB class

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,5 +1,7 @@
 
-let MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient
+const Address = require('../model/address')
+const Utxo = require('../model/utxo')
 
 class MongoDB {
 
@@ -8,19 +10,23 @@ class MongoDB {
   }
 
   connect (url, name) {
+    if (this.log > 2) console.log('MongoDB connect method')
     return MongoClient.connect(url)
       .then((mongoClient) => {
         this.mongoClient = mongoClient
         this.db = mongoClient.db(name)
         return Promise.resolve()
       })
+      .catch((err) => Promise.reject(err))
   }
 
   connected () {
+    if (this.log > 2) console.log('MongoDB connected method')
     return Promise.resolve(typeof this.db !== 'undefined')
   }
 
   close () {
+    if (this.log > 2) console.log('MongoDB close method')
     if (this.connected()) {
       return this.mongoClient.close()
     } else {
@@ -29,16 +35,20 @@ class MongoDB {
   }
 
   exists () {
-    return this.db.collections().then((collections) => {
-      if (collections.length === 2) {
-        return Promise.resolve(true)
-      } else {
-        return Promise.resolve(false)
-      }
-    });
+    if (this.log > 2) console.log('MongoDB exists method')
+    return this.db.collections()
+      .then((collections) => {
+        if (collections.length === 2) {
+          return Promise.resolve(true)
+        } else {
+          return Promise.resolve(false)
+        }
+      })
+      .catch((err) => Promise.reject(err))
   }
 
   init () {
+    if (this.log > 2) console.log('MongoDB init method')
     return Promise.all([
       this.db.createCollection('addresses')
         .then((addresses) => addresses.createIndex('address')),
@@ -47,27 +57,70 @@ class MongoDB {
     ])
   }
 
-  insertAddress (address) {
+  getLastExploredBlock () {
+    if (this.log > 2) console.log('MongoDB getLastExploredBlock method')
     let addresses = this.db.collection('addresses')
-    return addresses.insertOne(address)
+    return addresses.find().sort({lastSeen: -1}).limit(1).toArray()
+      .then((arr) => {
+        if (arr.length === 1) {
+          return Promise.resolve(arr[0].lastSeen)
+        } else {
+          return Promise.resolve(1)
+        }
+      })
+      .catch((err) => Promise.resolve(1))
+  }
+
+  insertAddress (address) {
+    if (this.log > 2) console.log('MongoDB insertAddress method')
+    let addresses = this.db.collection('addresses')
+    return addresses.insertOne(address, {w: 1, j:true})
+  }
+
+  updateAddress (addressObj) {
+    if (this.log > 2) console.log('MongoDB updateAddress method')
+    let addresses = this.db.collection('addresses')
+    return addresses.updateOne({address: addressObj.address}, addressObj, {w: 1, j:true})
   }
 
   findAddress (address) {
+    if (this.log > 2) console.log('MongoDB findAddress method')
     let addresses = this.db.collection('addresses')
     return addresses.findOne({address: address})
+      .then((addressObj) => Promise.resolve(Address.getInstance(addressObj)))
+      .catch((err) => Promise.resolve(Address.getInstance({})))
   }
 
   insertUtxo (utxo) {
+    if (this.log > 2) console.log('MongoDB insertUtxo method')
     let utxoset = this.db.collection('utxoset')
-    return utxoset.insertOne(utxo)
+    return utxoset.insertOne(utxo, {w: 1, j:true})
   }
 
-  findUtxo (txid, vout) {
+  updateUtxo (utxo) {
+    if (this.log > 2) console.log('MongoDB updateUtxo method')
+    let utxoset = this.db.collection('utxoset')
+    return utxoset.replaceOne({txid: utxo.txid}, utxo, {w: 1, j:true})
+  }
+
+  findUtxo (txid) {
+    if (this.log > 2) console.log('MongoDB findUtxo method')
     let utxoset = this.db.collection('utxoset')
     return utxoset.findOne({txid: txid})
+      .then((utxo) => Promise.resolve(Utxo.getInstance(utxo)))
+      .catch((err) => Promise.resolve(Utxo.getInstance({})))
+  }
+
+  removeUtxo (txid) {
+    if (this.log > 2) console.log('MongoDB removeUtxo method')
+    let utxoset = this.db.collection('utxoset')
+    return utxoset.deleteOne({txid: txid})
+      .then((response) => Promise.resolve(response.result.ok === 1))
+      .catch(() => Promise.resolve(false))
   }
 
   clean () {
+    if (this.log > 2) console.log('MongoDB clean method')
     if (typeof this.db !== 'undefined') {
       let p1 = this.db.dropCollection('addresses')
       let p2 = this.db.dropCollection('utxoset')
@@ -75,7 +128,6 @@ class MongoDB {
     } else {
       return Promise.resolve()
     }
-
   }
 }
 
--- test/database/mongodb.spec.js
+++ test/database/mongodb.spec.js
@@ -25,6 +25,28 @@ describe("MongoDB object test suite", function() {
       .then(() => done())
   })
 
+
+
+  it('checks getLastExploredBlock method', function(done) {
+
+    let addressObj = Address.getInstance({
+      address: 'testAddress',
+      lastSeen: 20
+    })
+
+    db.connect(url, dbName)
+      .then(() => db.init())
+      .then(() => db.getLastExploredBlock())
+      .then((lastExploredBlock) => expect(lastExploredBlock).toBe(1))
+      .then(() => db.insertAddress(addressObj))
+      .then(() => db.getLastExploredBlock())
+      .then((lastExploredBlock) => expect(lastExploredBlock).toBe(20))
+      .then(() => done())
+      .catch()
+      .then(() => db.clean())
+      .then(() => db.close())
+  })
+
   it('checks exists and init methods', function(done) {
 
     db.connect(url, dbName)
@@ -33,9 +55,10 @@ describe("MongoDB object test suite", function() {
       .then(() => db.init())
       .then(() => db.exists())
       .then((res) => expect(res).toBe(true))
+      .then(() => done())
+      .catch()
       .then(() => db.clean())
       .then(() => db.close())
-      .then(() => done())
   })
 
   it('checks insertAddress and findAddress methods', function(done) {
@@ -47,12 +70,22 @@ describe("MongoDB object test suite", function() {
       .then(() => db.insertAddress(addressObj))
       .then(() => db.findAddress('testAddress'))
       .then((addressFound) => expect(addressFound.address).toBe('testAddress'))
+      .then(() => done())
+      .catch()
       .then(() => db.clean())
+      .then(() => db.close())
+  })
+
+  it('checks updateAddress method', function(done) {
+
+    db.connect(url, dbName)
+      .then(() => db.connected())
+      .then((res) => expect(res).toBe(true))
       .then(() => db.close())
       .then(() => done())
   })
 
-  it('checks insertUtxo and findUtxo methods', function(done) {
+  it('checks insertUtxo, findUtxo and removeUtxo methods', function(done) {
 
     let utxo = Utxo.getInstance({
       txid: 'testId',
@@ -64,8 +97,11 @@ describe("MongoDB object test suite", function() {
       .then(() => db.insertUtxo(utxo))
       .then(() => db.findUtxo('testId'))
       .then((utxoFound) => expect(utxoFound.txid).toBe('testId'))
+      .then(() => db.removeUtxo('testId'))
+      .then((response) => expect(response).toBe(true))
+      .then(() => done())
+      .catch()
       .then(() => db.clean())
       .then(() => db.close())
-      .then(() => done())
   })
 })
